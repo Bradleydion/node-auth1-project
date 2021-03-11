4a131d9f1aab4b17285f994b87300900faa6d271
Bradley Dion <bradleydion@MacBook-Pro.local>
2021-03-10 22:27:02 -0500

    almost done' git push -u origin bradley-dion

--- api/server.js
+++ api/server.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const session = require("express-session")
 const KnexSessionStore = require("connect-session-knex")(session)
 const userRouter = require("./users/users-router")
+const authRouter = require("./auth/auth-router")
 const db = require("../data/db-config")
 
 
@@ -22,20 +23,34 @@ const db = require("../data/db-config")
 
 const server = express();
 
+const sessionConfig = {
+  name: 'chocolatechip',
+  secret: process.env.SESSION_SECRET || "chocolatechip",
+  cookie: {
+    maxAge: 1000 * 60 * 10,
+    secure: false,
+    httpOnly: true,
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new KnexSessionStore({
+    knex: db,
+    tableName: "sessions",
+    sidfieldname: "sid",
+    createtable: true,
+    clearInterval: 60000,
+  }),
+}
+
+
+
+
+
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
 server.use(express.json())
-server.use(session({
-  resave:false,
-  saveUninitialized:false,
-  secret:"chocolatechip",
-  store:new KnexSessionStore({
-    knex:db,
-    createtable:true,
-  })
-}))
-
+server.use(session(sessionConfig));
 
 server.get("/", (req, res) => {
   res.json({ api: "up" });
